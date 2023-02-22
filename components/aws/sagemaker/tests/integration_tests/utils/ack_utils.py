ad44a3a5cc266aa0032aca751fc0cd1a4807e15c
Ananth Bashyam <abashyam@amazon.com>
2023-02-22 19:25:11 +0000

    Hosting test

--- components/aws/sagemaker/tests/integration_tests/utils/ack_utils.py
+++ components/aws/sagemaker/tests/integration_tests/utils/ack_utils.py
@@ -7,7 +7,7 @@ def k8s_client():
     return config.new_client_from_config()
 
 
-def _get_resource(k8s_client, job_name, kvars):
+def _get_resource(k8s_client, job_name, plural):
     """Get the custom resource detail similar to: kubectl describe <resource> JOB_NAME -n NAMESPACE.
     Returns:
         None or object: None if the resource doesnt exist in server, otherwise the
@@ -16,22 +16,34 @@ def _get_resource(k8s_client, job_name, kvars):
     _api = client.CustomObjectsApi(k8s_client)
     namespace = os.environ.get("NAMESPACE")
     job_description = _api.get_namespaced_custom_object(
-        kvars["group"].lower(),
-        kvars["version"].lower(),
+        "sagemaker.services.k8s.aws",
+        "v1alpha1",
         namespace.lower(),
-        kvars["plural"].lower(),
+        plural,
         job_name.lower(),
     )
     return job_description
 
 
-def describe_training_job(k8s_client, training_job_name):
-    training_vars = {
-        "group": "sagemaker.services.k8s.aws",
-        "version": "v1alpha1",
-        "plural": "trainingjobs",
-    }
-    return _get_resource(k8s_client, training_job_name, training_vars)
+def _delete_resource(k8s_client, job_name, plural):
+    """Delete the custom resource
+    Returns:
+        None or object: None if the resource doesnt exist in server, otherwise the
+            custom object.
+    """
+    _api = client.CustomObjectsApi(k8s_client)
+    namespace = os.environ.get("NAMESPACE")
+    try:
+        _api.delete_namespaced_custom_object(
+            "sagemaker.services.k8s.aws",
+            "v1alpha1",
+            namespace.lower(),
+            plural,
+            job_name.lower(),
+        )
+    except:
+        return False
+    return True
 
 
 # TODO: Make this a generalized function for non-job resources.
@@ -39,7 +51,7 @@ def wait_for_trainingjob_status(
     k8s_client, training_job_name, desiredStatuses, wait_periods, period_length
 ):
     for _ in range(wait_periods):
-        response = describe_training_job(k8s_client, training_job_name)
+        response = _get_resource(k8s_client, training_job_name, "trainingjobs")
         if response["status"]["trainingJobStatus"] in desiredStatuses:
             return True
         sleep(period_length)
